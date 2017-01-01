ee0cb3b9018b8bf5952e9a77265243bd5b35ea88
noboyama <noboyama@yk.rim.or.jp>
2017-01-01 14:23:13 +0900

    ディレクトリ修正

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,8 @@ var gulp = require('gulp'); // gulpを読み込む
 var typescript = require('gulp-typescript');
 var sourcemaps = require('gulp-sourcemaps');
 
-var srcLib =["./typescript/lib/*.ts" ] ;
-var srcClient =["./typescript/client/*.ts" ] ;
-var srcServer =["./typescript/server/*.ts" ] ;
-
-gulp.task("lib" , function() {
-  gulp.src( srcLib )
-	.pipe( sourcemaps.init() )
-    .pipe(typescript())
-	.js
-	.pipe( sourcemaps.write() )
-    .pipe(gulp.dest("./public/js/"));
-});
+var srcClient =["./public/js/*.ts" ] ;
+var srcServer =["./routes/*.ts" ] ;
 
 gulp.task("client" , function() {
   gulp.src( srcClient )
@@ -35,7 +25,6 @@ gulp.task("server" , function() {
 
 
 gulp.task('watch', function (callback) {
-	gulp.watch(srcLib , ["lib"]) ;
 	gulp.watch(srcClient , ["client"]) ;
 	gulp.watch(srcServer , ["server"]) ;
 });
---
+++ routes/index.ts
@@ -0,0 +1,16 @@
+
+/// <reference path="./../node_modules/@types/express/index.d.ts" />
+/// <reference path="./../node_modules/@types/mongodb/index.d.ts" />
+/// <reference path="./../node_modules/@types/es6-promise/index.d.ts" />
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+	res.render('index', { title: 'Express' });  
+
+});
+
+module.exports = router;
